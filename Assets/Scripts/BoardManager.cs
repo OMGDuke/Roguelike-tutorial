5e0238debf1af28757954a567656e9695ea16c49
OMGDuke <lex1989@gmail.com>
2016-12-18 22:53:26 +0000

    Created player, enemy, floor, wall prefabs. Started board manager script

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count {
+		public int minimum;
+		public int maximum;
+
+		public Count(int min, int max) {
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count(5,9);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
